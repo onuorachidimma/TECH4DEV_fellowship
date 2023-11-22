faea1807c85950586c91d1333ac48201b0f224df
Chidimma Onuora <onuorachidimma4@gmail.com>
2023-11-22 02:06:10 +0100

    added the eye toggle functionality to password

--- form_task/assets/js/login.js
+++ form_task/assets/js/login.js
@@ -1,1 +1,21 @@
+var OpenEye = document.getElementById('eye1')
+var CloseEye = document.getElementById('eye2')
+var PassWord = document.getElementById('passWord')
+
+function toggleEye(){
+    if (PassWord.type === 'password'){
+        PassWord.type = 'text';
+        OpenEye.style.display ='block';
+        CloseEye.style.display = 'none';
+        
+    }
+    else{
+        PassWord.type = 'password';
+        OpenEye.style.display ='none';
+        CloseEye.style.display = 'block';
+    }
+}
+
+CloseEye.addEventListener('click', toggleEye)
+OpenEye.addEventListener('click', toggleEye)
 
